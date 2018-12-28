b58e2c0bce7d268f6853683374312f1ea5f51226
Marie Burns <mcburns2222@gmail.com>
2018-12-28 23:18:59 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,11 @@ class Scraper
   
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    binding.pry
   end
+end
+
+Scraper.new.get_page
   
   
   
